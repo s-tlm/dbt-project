230f55a301d18a1b99266c7445545c6d0acd738c
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-07-15 21:16:15 +1000

    Add credit card dimension

---
+++ slamco/models/marts/finance/dim_credit_card.sql
@@ -0,0 +1,19 @@
+with
+
+billing as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['billing_id']) }} as credit_card_key,
+        billing_id,
+        created_date,
+        holder_first_name,
+        holder_last_name,
+        card_type,
+        cvc_code,
+        card_number,
+        expiry_date,
+        expiry_month,
+        expiry_year
+    from {{ ref('int_billing_to_customer_joined') }}
+)
+
+select * from billing
